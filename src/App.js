d619cbfb44afbc229e23b9002ed1703b5d32b9de
Areeb Mustafa <amusta05@uuoguelph.ca>
2021-09-03 17:56:32 -0400

    fixing code however this is a backup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-
+  
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from './components/layout/Navbar';
 import Search from './components/searchs/Search';
+import axios from 'axios';
+import CountryCodes from './CountryCodes.json';
+import cityList from './city.list.json';
+
 function App() {
+  
+  const [loading,setLoading] = useState(false);
+  const [currentWeather,setCurrentWeather] = useState(null);
+  
+
+  const searchWeather = (city, country) => {
+    
+    
+    // let foundCountry = CountryCodes.codes.find(count => count.name===country);
+    // //console.log(foundCountry);
+    // console.log(city);
+    // console.log(foundCountry.code);
+    // let index = cityList.findIndex(function(item,i){
+      
+    //   return item.country === foundCountry.code && item.name ===city;
+    // });
+    
+  }
   
   return (
     <div>
       <Navbar title='Weather Application'/>
-      <Search title='Search'/>
+      <Search title='Search' searchWeather = {searchWeather}/>
     </div>
   );
 }
--- src/components/searchs/Search.js
+++ src/components/searchs/Search.js
@@ -1,48 +1,105 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-function Search({title}) {
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+import CountryCodes from '../../CountryCodes.json';
+function Search({title, searchWeather}) {
+
+    const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
+    //const [isUS, setISUS] = useState(false);
     const useStyles = makeStyles((theme) => ({
         root: {
           '& > *': {
+            
             margin: theme.spacing(1),
             width: '25ch',
           },
+          container: {
+            boxSizing: "border-box"
+          },
+
           size: {
               width: "500px"
+          },
+          formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120
+          },
+          selectEmpty: {
+            marginTop: theme.spacing(2)
           }
         },
     }));
+    const handleCity = event =>{
+        setCity(event.target.value);
+    }
+    const handleCountry = event => {
+        
+        setCountry(event.target.value);
+    }
+    const onClick = () =>{
+
+        searchWeather(city, country);
+    }
     const classes = useStyles();
     return (
-    <div>
+    <div class={classes.container}>
+        <div className={row}>
+        <div className={col75}>
             <form className={classes.root} noValidate autoComplete="off">
                 
-                <TextField id="outlined-basic" label="Enter city" variant="outlined" style={cityStyle}/>
-                <TextField id="outlined-basic" label="Enter country" variant="outlined" style={countryStyle}/>
+                <TextField id="outlined-basic" label="Enter city" variant="outlined" style={cityStyle} onChange={handleCity}/>
             </form>
-            <Button variant="contained"  style={buttonStyle}>
+        </div>
+        
+            <FormControl variant="outlined" className={classes.formControl}>
+                    <Select
+                        labelId="demo-simple-select-outlined-label"
+                        id="demo-simple-select-outlined"
+                        value={country}
+                        onChange={handleCountry}
+                        label="Age"
+                    >
+                    {
+                        CountryCodes.codes.map((a,b) => (
+                            
+                           <MenuItem value={a.name} key={b}> {a.name}</MenuItem>
+                        ))
+                    }
+                     </Select>
+             </FormControl>
+            <Button variant="contained"  onClick={onClick} style={buttonStyle}>
                 {title}
             </Button>
+
+            
     </div>
     
     )
 }
+const col75 = {
+    float: "right",
+    width: "75%",
+    marginTop: "6px"
+};
 const cityStyle = {
     width:"500px",
-    position: "absolute",
-    top: "10%",
-    right: "50%"   
+    resize: "vertical"
     
 };
-const countryStyle = {
-    width: "500px",
-    position: "absolute",
-    top: "10%",
-    right: "23%" 
-};
+const row ={
+    "&:after":{
+        content: "",
+        display: "table",
+        clear: "both"
+    }
+}
 const buttonStyle = {
     backgroundColor: "#fe4a49",
     color: "white",
